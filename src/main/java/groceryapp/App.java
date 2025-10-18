3f5b5a13c41c5cc17c7d60517bf3ac629dfae9b8
Hayden Olmstead <hayden.olmstead@gmail.com>
2025-10-17 23:32:27 -0600

    updating greeting

--- src/main/java/groceryapp/App.java
+++ src/main/java/groceryapp/App.java
@@ -8,15 +8,20 @@ import java.sql.Statement;
 
 /**
  * Main method for launching the application
+ * 
  * @param args
  */
 
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Groceries Application!");
-		
-		//Read in properties that are set
-		
-		//Start up the CLI and sit in a loop
+
+		// Read in properties that are set
+
+		// Start up the CLI and sit in a loop
+	}
+
+	public String printGreeting() {
+		return "Welcome to the groceries app!";
 	}
 }
